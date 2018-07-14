6780c9a46af50969699eaadb4ead51ddf4704cf4
JINxo <527382125@qq.com>
2018-07-14 16:56:01 +0800

    可以添加提货商

--- src/renderer/views/supplier/index.vue
+++ src/renderer/views/supplier/index.vue
@@ -19,7 +19,7 @@
  </el-table-column>
  <el-table-column
   prop="Supadress"
-  label="供货商地址地址">
+  label="供货商地址">
  </el-table-column>
  <el-table-column
  prop="Supphone"
@@ -33,21 +33,25 @@
     <el-button id="shuaxin" @click="fun2()" style="margin-left: 30%;margin-top: 3%;">刷新数据</el-button>
 
 
-    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+    <el-dialog title="添加供货商" :visible.sync="dialogFormVisible">
   <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" auto-complete="off"></el-input>
+    <el-form-item label="供货商编号" :label-width="formLabelWidth">
+      <el-input v-model="form.supcode" auto-complete="off"></el-input>
     </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
+        <el-form-item label="供货商名称" :label-width="formLabelWidth">
+      <el-input v-model="form.supname" auto-complete="off"></el-input>
     </el-form-item>
+        <el-form-item label="供货商地址" :label-width="formLabelWidth">
+      <el-input v-model="form.supadress" auto-complete="off"></el-input>
+    </el-form-item>
+            <el-form-item label="供货商电话" :label-width="formLabelWidth">
+      <el-input v-model="form.supphone" auto-complete="off"></el-input>
+    </el-form-item>
+
   </el-form>
   <div slot="footer" class="dialog-footer">
     <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="fun1()">添加</el-button>
+    <el-button type="button" @click="fun3()">添加</el-button>
   </div>
 </el-dialog>
 
@@ -68,8 +72,21 @@ fun1:function(){
 fun2:function(){
 
       this.axios.get("http://127.0.0.1:8080/search").then(body => {this.tableData = body.data;});
+},
+
+fun3:function(){
+this.dialogFormVisible = false;
+this.axios.post('http://127.0.0.1:8080/insert',this.form)
+.then(function() {
+console.log("接受成功");
+$("#shuaxin").click();
+})
+.catch(function(error) {
+console.log(error);
+});
 }
   },
+
     mounted() {
     /*         this.axios.post("http://rap2api.taobao.org/app/mock/15053/msg").then(body => {this.content = body.data; });    */
   this.axios.get("http://127.0.0.1:8080/search").then(body => {this.tableData = body.data;});
@@ -78,18 +95,15 @@ fun2:function(){
 
   data() {
     return {
+
       tableData: null,
        dialogTableVisible: false,
         dialogFormVisible: false,
         form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          supcode: '',
+          supname: '',
+          supadress: '',
+          supphone: '',
         },
         formLabelWidth: '120px'
     };
